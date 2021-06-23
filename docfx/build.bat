8f5a38dda182bcbaf0b6341c9dd3370019d52d87
Lorenzo Delana <lorenzo.delana@gmail.com>
2021-06-23 12:16:58 +0200

    doc

--- docfx/build.bat
+++
@@ -1,9 +0,0 @@
-cd %~dp0
-
-rd ..\docs /q /s
-
-docfx
-
-mkdir ..\docs\test
-
-xcopy /s ..\test ..\docs\test /exclude:exclude.txt
--- fixdocpath.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-exdir=$(dirname `readlink -f "$0"`)
-
-find "$exdir"/docs -type f -exec sed -i -e "s#file:///Z:/netcore-opengl/##g" {} \;
