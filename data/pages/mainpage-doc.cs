f3f485c8309385c3a43c00433afba09051e06cde
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-04-04 11:59:37 +0200

    doc

--- data/pages/mainpage-doc.cs
+++ data/pages/mainpage-doc.cs
@@ -6,9 +6,9 @@ This is the API documentation of the **netcore-opengl** library.
 - [Sources and Examples](https://github.com/devel0/netcore-opengl)
 
 Main modules and toolkits API :
-- \ref SearchAThing.OpenGL.Core "Core" ( \ref SearchAThing.OpenGL.Core.Toolkit "Toolkit" )
+- \ref SearchAThing.OpenGL.Core "Core" ( \ref SearchAThing.OpenGL.Core.Toolkit "Toolkit" - \ref SearchAThing.OpenGL.Core.Ext "Extensions" )
 - \ref SearchAThing.OpenGL.Shapes "Shapes" ( \ref SearchAThing.OpenGL.Shapes.Toolkit "Toolkit" )
-- \ref SearchAThing.OpenGL.Render "Render" ( \ref SearchAThing.OpenGL.Render.Toolkit "Toolkit" )
-- \ref SearchAThing.OpenGL.GUI "GUI" ( \ref SearchAThing.OpenGL.GUI.Toolkit "Toolkit" )
+- \ref SearchAThing.OpenGL.Render "Render" ( \ref SearchAThing.OpenGL.Render.Toolkit "Toolkit" - \ref SearchAThing.OpenGL.Render.Ext "Extensions" )
+- \ref SearchAThing.OpenGL.GUI "GUI" ( \ref SearchAThing.OpenGL.GUI.Toolkit "Toolkit" - \ref SearchAThing.OpenGL.GUI.Ext "Extensions" )
 
 */
