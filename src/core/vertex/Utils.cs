bbf29659ff10c13902d4e202bd9efb3af93cc232
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-04-10 19:52:52 +0200

    gl vertex position signature and make dict utils

---
+++ src/core/vertex/Utils.cs
@@ -0,0 +1,48 @@
+namespace SearchAThing.OpenGL.Core;
+
+public static partial class Ext
+{
+
+    /// <summary>
+    /// Retrieve vector3 position signature within given tolerance.
+    /// </summary>    
+    public static string PositionSignature(this Vector3 position, float tol) =>
+        Invariant($"{position.X.MRound(tol)}_{position.Y.MRound(tol)}_{position.Z.MRound(tol)}");
+
+    /// <summary>
+    /// Retrieve vertex position signature within given tolerance.
+    /// </summary>    
+    public static string PositionSignature(this IGLVertex glVertex, float tol) =>
+        glVertex.Position.PositionSignature(tol);
+
+    /// <summary>
+    /// From given figure create a vertex position based dictionary suitable to be queries through vertex
+    /// <see cref="PositionSignature"/>.
+    /// </summary>
+    /// <param name="figure">Gl figure which retrieve vertexes.</param>
+    /// <param name="tol">Vertex position tolerance to group them.</param>
+    /// <returns>Dictionary gl vertex position signature to list of gl vertexes within that position.</returns>
+    public static Dictionary<string, List<GLVertex>> BuildVertexPosDict(this IGLFigure figure, float tol) =>
+        new[] { figure }.BuildVertexPosDict(tol);
+
+    /// <summary>
+    /// From given list of figures create a vertex position based dictionary suitable to be queries through vertex
+    /// <see cref="PositionSignature"/>.
+    /// </summary>
+    /// <param name="figures">Gl figures which retrieve vertexes.</param>
+    /// <param name="tol">Vertex position tolerance to group them.</param>
+    /// <returns>Dictionary gl vertex position signature to list of gl vertexes within that position.</returns>
+    public static Dictionary<string, List<GLVertex>> BuildVertexPosDict(this IEnumerable<IGLFigure> figures, float tol) =>
+        figures
+            .SelectMany(fig => fig.Vertexes())
+            .Select(vtx => new
+            {
+                vtx,
+                vtxPosSignature = vtx.PositionSignature(tol)
+            })
+            .GroupBy(grp => grp.vtxPosSignature)
+            // distinct gl vertex by ref ( content uniqueness already ensure by gl vertex manager from where they come )
+            .ToDictionary(grp => grp.Key, grp => grp.Distinct().Select(g => g.vtx).ToList());
+
+
+}
