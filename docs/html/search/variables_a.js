5b396c3360c3d2069b3fd1778812332bba855c50
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-08-21 20:50:21 +0200

    doc

--- docs/html/search/variables_a.js
+++ docs/html/search/variables_a.js
@@ -6,8 +6,8 @@ var searchData=
   ['panrightgesture_3',['PanRightGesture',['../class_search_a_thing_1_1_open_g_l_1_1_g_u_i_1_1_avalonia_g_l_control.html#ada46dc8413837b3a1a648b3cc81ba531',1,'SearchAThing::OpenGL::GUI::AvaloniaGLControl']]],
   ['panupgesture_4',['PanUpGesture',['../class_search_a_thing_1_1_open_g_l_1_1_g_u_i_1_1_avalonia_g_l_control.html#a0aebf30e936f88c91600760810bfbf4e',1,'SearchAThing::OpenGL::GUI::AvaloniaGLControl']]],
   ['pastesimplecmdgesture_5',['PasteSimpleCmdGesture',['../class_search_a_thing_1_1_open_g_l_1_1_g_u_i_1_1_avalonia_g_l_control.html#a28c4a95fba62c568763d400f445a93da',1,'SearchAThing::OpenGL::GUI::AvaloniaGLControl']]],
-  ['position_6',['Position',['../struct_search_a_thing_1_1_open_g_l_1_1_core_1_1_g_l_vertex_struct.html#a1a9a536dcc6338f318a4a3f4f146d28b',1,'SearchAThing::OpenGL::Core::GLVertexStruct']]],
-  ['position_7',['position',['../struct_search_a_thing_1_1_open_g_l_1_1_core_1_1_g_l_point_light_struct.html#acfb76c74e507fea066625f69fbc8a146',1,'SearchAThing::OpenGL::Core::GLPointLightStruct']]],
+  ['position_6',['position',['../struct_search_a_thing_1_1_open_g_l_1_1_core_1_1_g_l_point_light_struct.html#acfb76c74e507fea066625f69fbc8a146',1,'SearchAThing::OpenGL::Core::GLPointLightStruct']]],
+  ['position_7',['Position',['../struct_search_a_thing_1_1_open_g_l_1_1_core_1_1_g_l_vertex_struct.html#a1a9a536dcc6338f318a4a3f4f146d28b',1,'SearchAThing::OpenGL::Core::GLVertexStruct']]],
   ['primitivesobc_8',['PrimitivesOBC',['../class_search_a_thing_1_1_open_g_l_1_1_core_1_1_g_l_figure_type_base.html#a6978385ec97b7be35e43900aaf1037c6',1,'SearchAThing::OpenGL::Core::GLFigureTypeBase']]],
   ['program_9',['Program',['../class_search_a_thing_1_1_open_g_l_1_1_core_1_1_g_l_pipeline.html#a76d431b352ca591c0e2e6e57bfdf9be5',1,'SearchAThing::OpenGL::Core::GLPipeline']]]
 ];
